e8544cfa89a9baebff02cd84b8cb832725912a58
guilhermeguerradev <guilhermeguerrajf@gmail.com>
2025-08-27 18:09:10 -0300

    feat: implement Board, Piece and Position classes with board printing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import boardgame.Position;
+import chesslayer.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
